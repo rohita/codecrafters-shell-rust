48851cb07d4847cbb4b6a468456164920ec56840
Rohit Agarwal <rohit.net@gmail.com>
2025-01-25 12:30:25 -0800

    The type builtin

--- src/builtin.rs
+++ src/builtin.rs
@@ -1,13 +1,32 @@
-use crate::builtin::Builtin::Echo;
+use std::process::exit;
+use crate::builtin::Builtin::*;
 
 pub enum Builtin {
     Echo,
+    Exit,
+    Type,
+    NotFound(String),
 }
 
 impl Builtin {
+    pub fn from(command: &str) -> Builtin {
+        match command {
+            "echo" => Echo,
+            "exit" => Exit,
+            "type" => Type,
+            _ => NotFound(command.to_string()),
+        }
+    }
+
     pub fn handle(&self, args: Vec<&str>) {
         match self {
             Echo => println!("{}", args.join(" ")),
+            Exit => exit(args[0].parse().unwrap()),
+            NotFound(command) => println!("{}: command not found", command),
+            Type => match args[0] {
+                "exit" | "echo" | "type" => println!("{} is a shell builtin", args[0]),
+                _ => println!("{}: not found", args[0]),
+            },
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+mod builtin;
+
+use crate::builtin::Builtin;
 #[allow(unused_imports)]
 use std::io::{self, Write};
-use std::process::exit;
 
 fn main() {
     let stdin = io::stdin();
@@ -12,14 +14,9 @@ fn main() {
         io::stdout().flush().unwrap();
 
         stdin.read_line(&mut input).unwrap();
-        let split_input = input.trim().split_whitespace().collect::<Vec<&str>>();
+        let split_input: Vec<_> = input.trim().split_whitespace().collect();
         let command = split_input[0];
         let args = split_input[1..].to_vec();
-
-        match command {
-            "echo"  => println!("{}", args.join(" ")),
-            "exit"  => exit(args[0].parse().unwrap()),
-            _       => println!("{}: command not found", command)
-        }
+        Builtin::from(command).handle(args);
     }
 }
